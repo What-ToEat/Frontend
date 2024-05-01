0bdf07f8cf3d7caf5a47af98ac56ac2ae09e4d0c
gemstoneyang <bennyws99@gmail.com>
2024-05-01 10:57:19 +0900

    Setting: React & Recoil 개발환경 세팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { atom, useRecoilState } from 'recoil';
+
+// 상태 정의
+const textState = atom({
+  key: 'textState', // 고유한 ID
+  default: '', // 기본값
+});
 
 function App() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      입력한 텍스트: {text}
     </div>
   );
 }
-
 export default App;
