a837d35f60e10470739c1c35fd272f394b4fe5e4
gemstoneyang <bennyws99@gmail.com>
2024-05-14 12:20:28 +0900

    Feat: 식당 필터링 기능 추가
    - 선택된 지역과 태그에 해당하는 식당을 필터링하여 조회
    
    - mock 데이터 분리

--- src/components/RestaurantView/RestaurantView.js
+++ src/components/RestaurantView/RestaurantView.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { selectedTagsState, selectedNavItemState } from '../../recoil/state';
 import RestaurantCard from '../RestaurantCard/RestaurantCard';
 import './RestaurantView.css';
 
 const RestaurantView = ({ restaurants }) => {
+  const selectedTags = useRecoilValue(selectedTagsState);
+  const selectedNavItem = useRecoilValue(selectedNavItemState);
+
+  // 필터링 함수 정의
+  const filteredRestaurants = restaurants.filter((restaurant) => {
+    const isNavItemIncluded = selectedNavItem === '전체' || restaurant.tags.includes(selectedNavItem);
+    const areAllTagsIncluded = selectedTags.every(tag => restaurant.tags.includes(tag));
+
+    return isNavItemIncluded && areAllTagsIncluded;
+  });
+
   return (
-		<div className='restaurant-view'>
-			<div className='restaurant-grid-title'>
-				# 여긴 어때요?
-			</div>
-			<div className="restaurant-grid">
-				{restaurants.map((restaurant, index) => (
-					<div className="restaurant-grid-item" key={index}>
-						<RestaurantCard 
-							name={restaurant.name} 
-							thumbnail={restaurant.thumbnail} 
-							tags={restaurant.tags}
-						/>
-					</div>
-				))}
-			</div>
-		</div>
+    <div className='restaurant-view'>
+      <div className='restaurant-grid-title'>
+        # 여긴 어때요?
+      </div>
+      <div className="restaurant-grid">
+        {filteredRestaurants.map((restaurant, index) => (
+          <div className="restaurant-grid-item" key={index}>
+            <RestaurantCard 
+              name={restaurant.name} 
+              thumbnail={restaurant.thumbnail} 
+              tags={restaurant.tags}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
--- src/pages/ViewPage/ViewPage.js
+++ src/pages/ViewPage/ViewPage.js
@@ -6,37 +6,17 @@ import SearchBar from '../../components/SearchBar/SearchBar'
 import TagNav from '../../components/TagNav/TagNav';
 import TagList from '../../components/TagList/TagList';
 
-const ViewPage = () => {
-	// Mock Data
-  const restaurantInfo = {
-    name: "맛있는 식당",
-    thumbnail: "https://img.hankyung.com/photo/202103/0Q.25813444.1.jpg",
-    tags: ["맛집", "맛집", "맛집", "맛집", "맛집", "맛집", "맛집"]
-  };
-  const restaurantInfo2 = {
-    name: "경주에서 제일 맛있는 초초초초초맛집",
-    thumbnail: "https://www.gyeongju.go.kr/upload/content/thumb/20201228/5DE22B6931D74EF9B333A081342A41F4.jpg",
-    tags: ["데이트", "가족과 함께", "가족과 함께", "한식", "경주"]
-  };
-  const restaurants = [restaurantInfo, restaurantInfo2, restaurantInfo, restaurantInfo2, restaurantInfo, restaurantInfo2, restaurantInfo, restaurantInfo2, restaurantInfo, restaurantInfo2, restaurantInfo, restaurantInfo2];
+import { mockRestaurants, mockTags } from './mock.js';
 
-  const tags = [
-    '트렌디', '독특한 인테리어', '가족 모임', '특별한 날', '힙한', '가성비',
-    '반려동물 동반', '생일 파티', '야외석', '혼자 오기 좋은', '이탈리안', '요즘 핫한',
-    '술집', '캐주얼', 'TV 출연 맛집', '프리미엄', '로맨틱한 분위기', '전통적인',
-    '비건 옵션', '해산물 전문', '브런치 카페', '라이브 음악', '뷔페 스타일', '퓨전 요리',
-    '저녁 데이트', '어린이 친화적', '뷰가 좋은', '레트로 감성', '유기농 재료', '공원 근처',
-    '야경이 멋진', '현지인 추천', '스페셜 메뉴', '바베큐', '디저트 카페', '채식주의자 친화적',
-    '지역 특산물', '새벽까지 영업', '주말 브런치', '계절 한정 메뉴'
-  ];
+const ViewPage = () => {
 
   return (
     <div className='view-page'>
       <Logo/>
       <SearchBar/>
       <TagNav/>
-      <TagList tags={tags}/>
-			<RestaurantView restaurants={restaurants}/>
+      <TagList tags={mockTags}/>
+			<RestaurantView restaurants={mockRestaurants}/>
 		</div>
   );
 };
