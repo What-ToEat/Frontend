edaa78729ad80a120e7063f8cb78b16ad8666c64
gemstoneyang <bennyws99@gmail.com>
2024-05-16 11:02:38 +0900

    Feat: 태그 목록 API 연결
    - useEffect를 사용해 최초 렌더링 시 태그 목록을 가져옴
    - 추후 확장성을 고려해 SelectableTags에 카테고리 항목을 추가

--- src/pages/ViewPage/ViewPage.js
+++ src/pages/ViewPage/ViewPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ViewPage.css';
 import RestaurantView from '../../components/RestaurantView/RestaurantView';
 import Logo from '../../components/Logo/Logo'
@@ -6,21 +6,27 @@ import SearchBar from '../../components/SearchBar/SearchBar'
 import TagNav from '../../components/TagNav/TagNav';
 import TagList from '../../components/TagList/TagList';
 
-import { mockRestaurants, mockTags } from './mock.js';
+import { mockRestaurants } from './mock.js';
 
 const ViewPage = () => {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    fetch("http://43.202.161.19:8080/api/tags")
+      .then(response => response.json())
+      .then(({data: {tags}}) => setTags(tags))
+      .catch(error => console.error('Error fetching tags:', error));
+  }, []);
 
   return (
     <div className='view-page'>
       <Logo/>
       <SearchBar/>
       <TagNav/>
-      <TagList tags={mockTags}/>
+      <TagList tags={tags}/>
 			<RestaurantView restaurants={mockRestaurants}/>
 		</div>
   );
 };
 
 export default ViewPage;
-
-
