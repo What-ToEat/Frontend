9a0fa488d549a9c5bab001779fca2e6e066982e9
gemstoneyang <bennyws99@gmail.com>
2024-05-16 15:39:59 +0900

    Fix: 모바일 스크롤 문제 해결

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ViewPage from './pages/ViewPage/ViewPage';
 import VotePage from './pages/VotePage/VotePage';
 
 function App() {
+  useEffect(() => {
+    function setScreenSize() {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    setScreenSize();
+    window.addEventListener('resize', setScreenSize);
+
+    // Cleanup function
+    return () => {
+      window.removeEventListener('resize', setScreenSize);
+    };
+  }, []);
 
   return (
     <Router>
       <div className="app">
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/view" element={<ViewPage/>} />
-          <Route path="/vote" element={<VotePage/>} />
-        </Routes>
+        <div className="viewer">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/view" element={<ViewPage />} />
+            <Route path="/vote" element={<VotePage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
 }
+
 export default App;
