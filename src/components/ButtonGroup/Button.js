f414cbdc48ddb031b53f74f532055ebabb0e9ac6
gemstoneyang <bennyws99@gmail.com>
2024-05-21 17:14:28 +0900

    Feat: 홈 화면 버튼 디자인

--- src/components/ButtonGroup/Button.js
+++ src/components/ButtonGroup/Button.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import './Button.css';
 
-const Button = ({ label, isSelected, onClick }) => {
+const Button = ({ label, isSelected, onClick, imageSrc }) => {
   return (
-    <button 
-      onClick={onClick}
-      style={{ backgroundColor: isSelected ? 'orange' : 'white' }}
-    >
-      {label}
-    </button>
+    <div className='home-button-container'>
+      <button
+        className='home-button'
+        onClick={onClick}
+        style={{
+          border: isSelected ? '3px solid #F77248' : '1px solid #C3C3C3',
+          backgroundImage: `url(${imageSrc})`,
+          backgroundSize: '80%',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
+      >
+      </button>
+      <p className='home-button-content'>{label}</p>
+    </div>
   );
 }
 
--- src/components/ButtonGroup/ButtonGroup.js
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import Button from './Button';
 
-const regions = ['강남', '홍대', '잠실', '건대', '신촌'];
-const tags = ['분위기 좋은 카페', '맛있는 베이커리', '데이트하기 좋은 레스토랑', '오늘부터 다이어트', '달려 보자! 회식 장소'];
+const regions = [
+  { name: '강남', image: '/images/gangnam.png' },
+  { name: '홍대', image: '/images/hongdae.png' },
+  { name: '잠실', image: '/images/jamsil.png' },
+  { name: '건대', image: '/images/gundae.png' },
+  { name: '신촌', image: '/images/shinchon.png' }
+];
+
+const tags = [
+  { name: '분위기 좋은 카페', image: '/images/cafe.png' },
+  { name: '맛있는 베이커리', image: '/images/bakery.png' },
+  { name: '데이트하기 좋은 레스토랑', image: '/images/date.png' },
+  { name: '오늘부터 다이어트', image: '/images/diet.png' },
+  { name: '달려 보자! 회식 장소', image: '/images/party.png' }
+];
 
 const ButtonGroup = ({ selectedRegion, setSelectedRegion, selectedTag, setSelectedTag }) => {
   return (
@@ -11,9 +24,10 @@ const ButtonGroup = ({ selectedRegion, setSelectedRegion, selectedTag, setSelect
         {regions.map((region, index) => (
           <Button 
             key={index}
-            label={region}
-            isSelected={selectedRegion === region}
-            onClick={() => setSelectedRegion(region)}
+            label={region.name}
+            isSelected={selectedRegion === region.name}
+            onClick={() => setSelectedRegion(region.name)}
+            imageSrc={region.image}
           />
         ))}
       </div>
@@ -21,9 +35,10 @@ const ButtonGroup = ({ selectedRegion, setSelectedRegion, selectedTag, setSelect
         {tags.map((tag, index) => (
           <Button 
             key={index}
-            label={tag}
-            isSelected={selectedTag === tag}
-            onClick={() => setSelectedTag(tag)}
+            label={tag.name}
+            isSelected={selectedTag === tag.name}
+            onClick={() => setSelectedTag(tag.name)}
+            imageSrc={tag.image}
           />
         ))}
       </div>
