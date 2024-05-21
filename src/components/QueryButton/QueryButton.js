9de9343e78c83f54841158d06305a77799523837
gemstoneyang <bennyws99@gmail.com>
2024-05-21 18:03:53 +0900

    Feat: 버튼 비활성화 디자인 추가

--- src/components/QueryButton/QueryButton.js
+++ src/components/QueryButton/QueryButton.js
@@ -6,6 +6,9 @@ const QueryButton = ({ isEnabled }) => {
     <button className='home-query-button' disabled={!isEnabled}>
       조&nbsp;&nbsp;&nbsp;회
     </button>
+    // 버튼이 disable일 때의 디자인을 추가해줘.
+    
+
   );
 }
 
