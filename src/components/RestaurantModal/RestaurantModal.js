efd3c05e53752cce113f0073e4cb2d5b6a067739
gemstoneyang <bennyws99@gmail.com>
2024-06-01 18:56:04 +0900

    Feat: 모달에 태그 목록 추가

--- src/components/RestaurantModal/RestaurantModal.js
+++ src/components/RestaurantModal/RestaurantModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import Tag from '../Tag/Tag';  // Tag 컴포넌트를 임포트합니다.
 
 const RestaurantModal = ({ show, handleClose, restaurant }) => {
   return (
@@ -9,12 +10,12 @@ const RestaurantModal = ({ show, handleClose, restaurant }) => {
       </Modal.Header>
       <Modal.Body>
         <img src={restaurant.thumbnail} alt={restaurant.name} style={{ width: '100%' }} />
-        <div>
+        <div style={{ maxHeight: '100px', overflowY: 'scroll', marginTop: '10px' }}>
           {restaurant.tags.map(tag => (
-            <span key={tag.name} className="badge badge-secondary">{tag.name}</span>
+            <Tag key={tag.name} name={tag.name} category={tag.category} />
           ))}
         </div>
-        <div>
+        <div style={{ marginTop: '10px' }}>
           <h5>리뷰</h5>
           <ul>
             {restaurant.reviews.map((reviewObj, index) => (
