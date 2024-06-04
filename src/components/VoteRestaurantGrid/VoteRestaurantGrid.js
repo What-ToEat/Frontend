549023277df5b7aded9f908bc158cd7944c99803
gemstoneyang <bennyws99@gmail.com>
2024-06-04 14:29:20 +0900

    Feat: 투표 grid 디자인

--- src/components/VoteRestaurantGrid/VoteRestaurantGrid.js
+++ src/components/VoteRestaurantGrid/VoteRestaurantGrid.js
@@ -4,12 +4,20 @@ import './VoteRestaurantGrid.css';
 
 const VoteRestaurantGrid = ({ restaurants, onAddClick }) => {
   return (
-    <div className="restaurant-grid">
-      {restaurants.map((restaurant, index) => (
-        <RestaurantCard key={index} {...restaurant} />
-      ))}
-      <div className="restaurant-card add-card" onClick={onAddClick}>
-        <span>+</span>
+    <div className='vote-restaurant-grid-container'>
+      <div className="vote-restaurant-grid">
+        {restaurants.map((restaurant, index) => (
+          <div className="vote-restaurant-grid-item" key={index}>
+            <RestaurantCard key={index} {...restaurant} />
+          </div>
+        ))}
+        <div className="vote-restaurant-grid-item" onClick={onAddClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="186" height="177" viewBox="0 0 186 177" fill="none">
+            <rect x="0.5" y="0.5" width="185" height="176" rx="4.5" fill="white" stroke="#C3C3C3"/>
+            <line x1="79" y1="89" x2="107" y2="89" stroke="#C3C3C3" stroke-width="2"/>
+            <path d="M93 75L93 103" stroke="#C3C3C3" stroke-width="2"/>
+          </svg>
+        </div>
       </div>
     </div>
   );
