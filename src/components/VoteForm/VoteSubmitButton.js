590bdfa3a264b737b197bb99144d839876d20092
gemstoneyang <bennyws99@gmail.com>
2024-06-04 14:08:13 +0900

    Style: 메인 페이지 추가 디자인
    - footer 디자인
    - 폼 컴포넌트 간격 조정

--- src/components/VoteForm/VoteSubmitButton.js
+++ src/components/VoteForm/VoteSubmitButton.js
@@ -4,7 +4,7 @@ import './VoteSubmitButton.css';
 const VoteSubmitButton = ({ onClick }) => {
   return (
     <button className="vote-submit-button" type="button" onClick={onClick}>
-      투표 시작하기
+      <p>투표 시작하기</p>
     </button>
   );
 };
