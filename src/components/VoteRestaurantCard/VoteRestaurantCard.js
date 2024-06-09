01fe8e5bebec11990926068129e658936ac79b67
gemstoneyang <bennyws99@gmail.com>
2024-06-09 15:52:47 +0900

    Feat: 현재 투표 인원 확인을 위한 툴팁 추가

--- src/components/VoteRestaurantCard/VoteRestaurantCard.js
+++ src/components/VoteRestaurantCard/VoteRestaurantCard.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './VoteRestaurantCard.css';
 
 const VoteRestaurantCard = ({ option, onClick, isSelected, onViewDetails }) => {
+  const [showVoterList, setShowVoterList] = useState(false);
+  const voterListRef = useRef(null);
+
+  const handleToggleVoterList = (e) => {
+    e.stopPropagation();
+    if (option.voterList.length > 0) {
+      setShowVoterList(!showVoterList);
+    }
+  };
+
+  const handleClickOutside = (event) => {
+    if (voterListRef.current && !voterListRef.current.contains(event.target)) {
+      setShowVoterList(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showVoterList) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [showVoterList]);
+
   return (
     <div className={`vote-restaurant-card ${isSelected ? 'vote-restaurant-card-selected' : ''}`} onClick={() => onClick(option.restaurantId)}>
       <div className="vote-restaurant-card-left">
@@ -13,10 +40,21 @@ const VoteRestaurantCard = ({ option, onClick, isSelected, onViewDetails }) => {
       <div className="vote-restaurant-card-right">
         <div className="vote-restaurant-card-details">
           <strong className="vote-restaurant-card-name">{option.restaurantName}</strong>
-          <div className="vote-restaurant-card-voters">
+          <div className="vote-restaurant-card-voters" onClick={handleToggleVoterList}>
             <span className="vote-restaurant-card-voters-icon">👤</span>
             <span>득표수: {option.voterList.length}</span>
           </div>
+          {showVoterList && (
+            <div className="vote-restaurant-card-voter-list-tooltip" ref={voterListRef}>
+              <ul className="vote-restaurant-card-voter-list">
+                {option.voterList.map((voter, index) => (
+                  <li key={index} className="vote-restaurant-card-voter">
+                    <p>{voter.nickname}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
         <button onClick={(e) => { e.stopPropagation(); onViewDetails(option); }} className="vote-restaurant-card-button">상세 정보</button>
       </div>
